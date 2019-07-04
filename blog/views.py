158a2905135ec3bd788261899c1b71f75046fe58
well <luwei_whu@163.com>
2019-07-04 14:39:17 +0800

    Hello world,coming

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world!")
+    return HttpResponse("Hello world! I'm coming")
 
